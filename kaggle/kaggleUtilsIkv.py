33fdd8c246e9d0599fb57412673e15d4ac55fd9d
vibhav <ikvibhav@gmail.com>
2025-06-22 15:37:52 +0200

    DASC: 22062025 changes v1

--- kaggle/kaggleUtilsIkv.py
+++ kaggle/kaggleUtilsIkv.py
@@ -1,19 +1,22 @@
 import pandas as pd
 
+
 def perform_explore(df: pd.DataFrame) -> None:
-    print(f'------df.head()--------')
+    print(f"------df.head()--------")
     print(df.head())
-    print(f'------df.info()--------')
+    print(f"------df.info()--------")
     print(df.info())
-    print(f'------df.describe()--------')
+    print(f"------df.describe()--------")
     print(df.describe())
-    print(f'------df.shape--------')
+    print(f"------df.shape--------")
     print(df.shape)
 
 
-def perform_correlation_matrix(df: pd.DataFrame, feature_list: list = None) -> pd.DataFrame | None:
+def perform_correlation_matrix(
+    df: pd.DataFrame, feature_list: list = None
+) -> pd.DataFrame | None:
     try:
         return pd.get_dummies(df[feature_list]).corr()
     except Exception as e:
-        print(f'Error: {e}')
+        print(f"Error: {e}")
         return None
